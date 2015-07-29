0860d4e3a79b70dfe1b2e310529dc63a8c608cf3
Emily Elmore <emilyelmore@Emilys-iMac.home>
2015-07-29 10:33:01 -0500

    changes

--- wp-content/themes/accelerate-theme-child/single-case_studies.php
+++ wp-content/themes/accelerate-theme-child/single-case_studies.php
@@ -9,12 +9,12 @@
  */
 
 get_header(); ?>
-$size = "full";
 <div id="primary" class="site-content">
 		<div id="content" role="main">
 		
 			<?php while ( have_posts() ) : the_post();
 				1
+				$size = "full";
 				$services = get_field('services');
 				$client = get_field('client');
 				$link = get_field('site_link');
