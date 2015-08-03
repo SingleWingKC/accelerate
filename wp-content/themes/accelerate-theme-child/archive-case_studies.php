2c7de7f8dbda10c71f0aa20d69bbca8761a22f42
Emily Elmore <emilyelmore@Emilys-iMac.home>
2015-08-03 10:15:26 -0500

    fixed code

--- wp-content/themes/accelerate-theme-child/archive-case_studies.php
+++ wp-content/themes/accelerate-theme-child/archive-case_studies.php
@@ -23,7 +23,7 @@ get_header(); ?>
 	
 	<article class="case-study">
 		<aside class= "case-study-sidebar">
-			<h2><?php <?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+			<h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
 			<h5><?php echo $services; ?></h5>
 			<?php the_excerpt(); ?>
 			<p><strong><a href="<?php the_permalink(); ?>">View Project</a></strong></p>
